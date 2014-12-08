1c8c9a95f134fabd6cbc611dd5d9893cdebc3e3e
Andrej T <andrejtrajchevski@gmail.com>
2014-12-08 18:31:21 +0100

    all now accepts objects

--- invariant.js
+++ invariant.js
@@ -1,6 +1,19 @@
 /* Library for testing things you expect to always be satisfied */
 var colors = require('colors');
 
+var helpers = {
+    type: function(obj) {
+        var type = Object.prototype.toString.call(obj);
+        if (type === '[object Array]') {
+            return 'Array';
+        } else if (type === '[object Object]') {
+            return 'Object';
+        } else {
+            throw new Error('Unknown type: {{type}}'.replace('{{type}}', type));
+        }
+    }
+};
+
 function inv(description, f) {
     return function() {
         var result, pass = true;
@@ -18,6 +31,9 @@ function inv(description, f) {
             return true;
         } else if (result === false) {
             return false;
+        } else if (typeof result === 'undefined') {
+            console.warn(colors.yellow('No result returned from: "{{desc}}"'
+                                       .replace('{{desc}}', description)));
         } else {
             return pass;
         }
@@ -25,11 +41,19 @@ function inv(description, f) {
 }
 
 var all = function(invariants) {
-    return function() {
-        return invariants.reduce(function(prev, f) {
-            return f() && prev;
-        }, true);
-    };
+    if (helpers.type(invariants) === 'Array') {
+        return function() {
+            return invariants.reduce(function(prev, f) {
+                return f() && prev;
+            }, true);
+        };
+    } else if (helpers.type(invariants) === 'Object') {
+        var invArr = [];
+        for (var inv in invariants) {
+            invArr.push(invariants[inv]);
+        }
+        return all(invArr);
+    }
 };
 
 var when = function(condition, invariants) {
